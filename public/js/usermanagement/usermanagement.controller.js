b866b31317caad81f7e33aa7c5720d2971365474
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 19:13:16 +0800

    feat(usermanagement):handle creat daata

--- public/js/usermanagement/usermanagement.controller.js
+++ public/js/usermanagement/usermanagement.controller.js
@@ -3,7 +3,7 @@ import userManagementService from "./usermanagement.service.js";
 $(document).ready(function () {
     const usermanagementservice = new userManagementService();
     usermanagementservice.getAllData();
-    $(document).on('click', '.page-link' , function (e) {
+    $(document).on('click', '.page-link', function (e) {
         e.preventDefault();
         const url = new URL($(this).attr('href'));
         const fullUrl = url.pathname + url.search;
@@ -18,5 +18,63 @@ $(document).ready(function () {
 
     $(document).on('click', '#search-button', function () {
         usermanagementservice.getAllData();
+    });
+
+    function validation() {
+        $('#formTambah').validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                role: {
+                    required: true
+                }
+            },
+            messages: {
+                email: {
+                    required: 'Email tidak boleh kosong',
+                    email: 'Email tidak valid'
+                },
+                role: {
+                    required: 'Role tidak boleh kosong'
+                }
+            },
+            highlight: function (element) {
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error')
+            },
+
+            success: function (element) {
+                $(element).closest('.form-group').removeClass('has-error').addClass('has-success')
+            }
+        })
+    }
+
+    validation()
+
+    $('#email').on('input', function () {
+        $(this).valid()
+    })
+
+    $('#role').on('change', function () {
+        $(this).valid()
+    })
+
+    let isEditMode = false
+
+    function showModal(editModal = false, id = '') {
+        isEditMode = editModal
+        $('#modal-title').text('Edit Member');
+        $('.button-footer button[type="submit"]').text('Update');
+
+        $('#id').val(id);
+        console.log('disini idnya', id)
+        $('#userManagementModal').modal('show');
+    }
+
+
+    $('#formTambah').submit(function (e) {
+        e.preventDefault();
+        usermanagementservice.createData(e, isEditMode)
     });
 });
