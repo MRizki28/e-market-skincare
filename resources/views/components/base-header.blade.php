f26b63f6ab1d2cab9bcd5ae4069578d2685c849a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 17:27:56 +0800

    feat(usermanagement):crete user management

---
+++ resources/views/components/base-header.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="page-header d-flex justify-content-between align-items-center">
+        <h1 class="page-title"><i class="{{ $headerIcon }} mr-2"></i>{{ $headerName }}</h1>
+    </div>
+</div>
