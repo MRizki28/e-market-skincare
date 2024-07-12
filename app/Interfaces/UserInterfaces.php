dcd742110ba467192df9d57989a8f7ccd9b51544
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 18:55:32 +0800

    feat(usermanagement):create function for get data

--- app/Interfaces/UserInterfaces.php
+++ app/Interfaces/UserInterfaces.php
@@ -3,8 +3,10 @@
 namespace App\Interfaces;
 
 use App\Http\Requests\User\UserRequest;
+use Illuminate\Http\Request;
 
 interface UserInterfaces
 {
+    public function getAllData(Request $request);
     public function register(UserRequest $request);
 }
