e4a41e6b77e4e0d2cb1bacd88e228c10469bdb87
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 17:27:51 +0800

    feat(usermanagement):crete user management

---
+++ resources/views/components/base-body.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="m-0 p-0">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
