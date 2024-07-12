2ad9395af1607f68bf32e21f42b5f9ac5a16acd3
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 18:55:42 +0800

    feat(usermanagement):create function for get data

--- app/Repositories/UserRepositories.php
+++ app/Repositories/UserRepositories.php
@@ -6,6 +6,7 @@ use App\Http\Requests\User\UserRequest;
 use App\Interfaces\UserInterfaces;
 use App\Models\User;
 use App\Traits\HttpResponseTrait;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserRepositories implements UserInterfaces
@@ -16,6 +17,30 @@ class UserRepositories implements UserInterfaces
     public function __construct(User $userModel)
     {
         $this->userModel = $userModel;
+    }
+
+    public function getAllData(Request $request)
+    {
+        $search = $request->input('search');
+        $page = (int) $request->input('page', 1);
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+
+        $query = $this->userModel->query();
+
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('email', 'like', '%' . $search . '%')
+                    ->orWhere('role', 'like', '%' . $search . '%');
+            });
+        }
+
+        $data = $query->paginate($limit, ['*'], 'page', $page);
+
+        if ($data->isEmpty()) {
+            return $this->dataNotFound();
+        }else{
+            return $this->success($data, 'success', 'Success get data user');
+        }
     }
 
     public function register(UserRequest $request)
