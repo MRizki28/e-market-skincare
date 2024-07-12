4f154019614fffead21234cf9d985f330c785478
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 17:28:11 +0800

    feat(usermanagement):crete user management

---
+++ resources/views/Layouts/Base.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>e-skincare</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="key" content="{{ env('APP_KEY') }}">
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="{{ asset('static/img/logo.svg') }}" type="image/svg+xml" />
+    @include('Layouts.styles')
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="main-header">
+            <div class="logo-header position-relative" style="background-color: #FF4191">
+                <a href="/" class="logo ">
+                    <span class="navbar-brand text-light position-relative font-weight-bold ">e-skincare</span>
+                </a>
+                <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse"
+                    data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <i class="icon-menu"></i>
+                    </span>
+                </button>
+                <button class="topbar-toggler more"><i class="icon-options-vertical"></i></button>
+                <div class="nav-toggle">
+                    <button class="btn btn-toggle toggle-sidebar">
+                        <i class="icon-menu"></i>
+                    </button>
+                </div>
+            </div>
+            @include('Layouts.Navbar')
+        </div>
+
+        @include('Layouts.Sidebar')
+        <div class="main-panel">
+            <div class="content">
+                @yield('content')
+
+            </div>
+            @include('Layouts.footer')
+        </div>
+    </div>
+    @include('Layouts.scripts')
+    @yield('js-service')
+
+
+    <script type="module" src="{{ asset('js/firebase/firebase.service.js') }}"></script>
+</body>
+
+</html>
