6c2e8416c62b655a52b48379dffebbe7117e3165
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 18:55:57 +0800

    feat(usermanagement):create function for get data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::create([
+            'email' => 'adminskincare@mail.com',
+            'password' => Hash::make('123'),
+            'role' => 'admin'
+        ]);
     }
 }
