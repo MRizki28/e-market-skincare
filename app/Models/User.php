d334b0279dd7c0eb6552e121db142adc403b9265
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-12 17:26:32 +0800

    feat(usermanagement):crete user management

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,6 @@ class User extends Authenticatable
     use HasFactory, Notifiable, HasUuids;
     protected $table = 'users';
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
