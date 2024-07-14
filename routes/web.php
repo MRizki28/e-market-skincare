f5ca4390b8f616219b659ca881e4cf4e22535e6d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-14 19:41:13 +0800

    feat(usermanagement):handle get and update data

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,7 @@ Route::prefix('v1')->group(function() {
     Route::prefix('user')->controller(UserController::class)->group(function() {
         Route::get('/', 'getAllData');
         Route::post('register', 'register');
+        Route::get('/get/{id}', 'getDataById');
+        Route::post('update/{id}', 'updateData');
     });
 });
