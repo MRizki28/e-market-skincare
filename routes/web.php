74a95be770c4a224ca1d778b1494b442fb4b65be
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-18 20:12:11 +0800

    feat(distributor):create distributor

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,21 @@ use App\Http\Controllers\CMS\DistributorController;
 use App\Http\Controllers\CMS\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.usermanagement');
-});
+Route::get('/login', function () {
+    return view('Auth.login');
+})->name('login');
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/', function () {
+        return view('pages.usermanagement');
+    });
 
+    Route::get('/distributor', function() {
+        return view('pages.distributor');
+    });
+    
     Route::prefix('v1')->group(function () {
         Route::prefix('user')->controller(UserController::class)->group(function () {
             Route::get('/', 'getAllData');
