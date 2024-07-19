50f32e3b8ed4305a5485411aba5ef17e3a334484
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-19 20:08:50 +0800

    feat(product):create API fo product

--- database/migrations/2024_07_11_140550_create_master_product_table.php
+++ database/migrations/2024_07_11_140550_create_master_product_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tb_product', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('product_name', 50);
+            $table->string('product_image');
             $table->integer('price');
             $table->timestamps();
         });
