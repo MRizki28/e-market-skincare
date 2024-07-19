6774bbce457eb57fb7db826ff687573a93e2d19e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-19 20:08:36 +0800

    feat(product):create API fo product

---
+++ app/Interfaces/ProductInterfaces.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Http\Requests\Product\ProductRequest;
+use Illuminate\Http\Request;
+
+interface ProductInterfaces
+{
+    public function getAllData(Request $request);
+    public function createData(ProductRequest $request);
+    public function getDataById($id);
+    public function updateData(ProductRequest $request, $id);
+    public function deleteData($id);
+}
