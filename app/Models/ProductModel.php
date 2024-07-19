40ec9d2ba3bab088a78d1b38e37cd61ae58ba080
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-19 20:08:41 +0800

    feat(product):create API fo product

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'tb_product';
+    protected $fillable = [
+        'id',
+        'product_name',
+        'product_image',
+        'price'
+    ];
+}
