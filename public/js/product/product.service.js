4824fcf4962b7ff925ffc1c0227c3845bde8e104
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-20 17:52:43 +0800

    feat(product):render product admin

---
+++ public/js/product/product.service.js
@@ -0,0 +1,145 @@
+class productService {
+    async getAllData(url) {
+        const pagination = $('.pagination')
+        const table = $('#table tbody')
+        const dataNotFound = $('#dataNotFound')
+        const totalData = $('#data-total')
+
+        let params = $('#form-search').val();
+        let endpoint = paramsUrl(url || '/v1/product', { search: params });
+        const response = await axios.get(endpoint);
+        const responseData = await response.data;
+
+        table.empty();
+        pagination.empty();
+
+        let tableBody
+        if (responseData.message === 'Success get data product') {
+            $.each(responseData.data.data, function (index, item) {
+                tableBody += "<tr>";
+                tableBody += "<td>" + item.product_name + "</td>"
+                tableBody += "<td>" + "Rp. " + numberWithCommas(item.price) + "</td>"
+                tableBody += "<td>" + item.description + "</td>"
+                tableBody += "<td>" + "<a href='" + "uploads/product/" + item.product_image + "' target='_blank'><img src='" + "uploads/product/" + item.product_image + "' class='img-thumbnail' width='50' height='50'></a>" + "</td>"
+                tableBody +=
+                    "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                    "<button class='btn btn-sm edit-modal mr-1' data-toggle='modal' data-target='#productModal' data-id='" +
+                    item.id + "'><i class='fas fa-edit'></i></button>" +
+                    "<button type='submit' class='delete-confirm btn btn-sm' data-id='" +
+                    item.id + "'><i class='fas fa-trash-alt'></i></button>" +
+                    "</td>";
+                tableBody += "</tr>";
+                dataNotFound.hide()
+            });
+
+            table.append(tableBody);
+            paginationLink(pagination, responseData);
+            totalData.text(responseData.data.total);
+        } else {
+            table.empty()
+            dataNotFound.show()
+            pagination.empty()
+            totalData.text('0')
+        }
+
+    }
+
+    async createData(e, isEditMode, price) {
+        let submitButton = $(e.target).find(':submit')
+        try {
+            let formData = new FormData(e.target)
+            formData.append('price', price)
+
+            if (isEditMode) {
+                console.log(price)
+                let id = $('#id').val()
+                submitButton.attr('disabled', true)
+                const response = await axios.post(`/v1/product/update/${id}`, formData)
+                const responseData = await response.data
+                if (responseData.status == 'success') {
+                    successUpdateAlert().then(function () {
+                        $('#productModal').modal('hide')
+                    })
+                    this.getAllData()
+                    submitButton.attr('disabled', false)
+                }
+            } else {
+                submitButton.attr('disabled', true)
+                const response = await axios.post('/v1/product/create', formData)
+                const responseData = await response.data
+                if (responseData.status == 'success') {
+                    successAlert().then(function () {
+                        $('#productModal').modal('hide')
+                    })
+                    this.getAllData()
+                    submitButton.attr('disabled', false)
+                }
+            }
+        } catch (error) {
+            console.log(error);
+            submitButton.attr('disabled', false)
+            if (error.response.status == 422) {
+                warningAlert();
+            } else {
+                errorAlert();
+            }
+        };
+    }
+
+    async getDataById(id, isEditMode, updatePrice) {
+        try {
+            const response = await axios.get(`/v1/product/get/${id}`)
+            const responseData = await response.data
+            if (responseData.status == 'success') {
+                const priceAsString = responseData.data.price.toString();
+                const formattedPrice = formatCurrency(priceAsString);
+
+                updatePrice = responseData.data.price;
+                $('#product_name').val(responseData.data.product_name)
+                $('#price').val(formattedPrice)
+                $('#description').val(responseData.data.description)
+                $('#preview').attr('src', "uploads/product/" + responseData.data
+                    .product_image);
+
+                $('#product_image').on('change', function () {
+                    const file = $(this)[0].files[0];
+                    const fileReader = new FileReader();
+                    fileReader.onloadend = function () {
+                        $('#preview').attr('src', fileReader.result);
+                    }
+                    fileReader.readAsDataURL(file);
+                });
+
+                const fileUrl = "uploads/product/" + responseData.data.product_image;
+                const fileNames = fileUrl.split('/').pop();
+                const blob = await fetch(fileUrl).then(r => r.blob());
+                const file = new File([blob], fileNames);
+                const fileList = new DataTransfer();
+                fileList.items.add(file);
+                $('#product_image').prop('files', fileList.files);
+                isEditMode(true, responseData.data.id)
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async deleteData(id) {
+        try {
+            const response = await axios.delete('/v1/product/delete/' + id)
+            deleteAlert().then(async (result) => {
+                if (result.isConfirmed) {
+                    const responseData = await response.data
+                    if (responseData.status == 'success') {
+                        successDeleteAlert()
+                        this.getAllData()
+                    }
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default productService;
