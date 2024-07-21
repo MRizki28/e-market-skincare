df4d1ea4e140416bbc1a89055757fc78b5b9c3b9
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-21 20:41:55 +0800

    feat(frontend):setup react for fe

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react()
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
 });
