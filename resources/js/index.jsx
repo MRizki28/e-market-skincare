18675cd13e78fac98bf4ce11fa59a9fdf7ccaede
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-22 18:36:06 +0800

    feat(frontend):update ui

--- resources/js/index.jsx
+++ resources/js/index.jsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import '../css/app.css';
 import './bootstrap';
 import 'flowbite'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const root = ReactDOM.createRoot(document.getElementById('frontend-app'));
 
