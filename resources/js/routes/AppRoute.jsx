3c5aa070cd10f20c96201fef4d223a856f295a5e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-24 16:30:18 +0800

    feat(frontend):create route auth for frontend

--- resources/js/routes/AppRoute.jsx
+++ resources/js/routes/AppRoute.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../ui/home/Home";
+import { Auth } from "../ui/auth/Auth";
 
 export default function AppRoute() {
     return (
         <Routes>
-            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/login" element={<Auth></Auth>}></Route>
+            <Route path="/" element={
+                <Home></Home>
+            }></Route>
         </Routes>
     )
 }
