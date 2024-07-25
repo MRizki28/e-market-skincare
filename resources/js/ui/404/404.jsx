705a1797e9de0edde0607006f74f6976b7d86e6b
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-25 15:55:38 +0800

    feat(frontend):create detail product

---
+++ resources/js/ui/404/404.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        </div>
+    );
+}
