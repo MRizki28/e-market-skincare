11ee6857ca28d7467f96d875ea552d348d4d629b
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-29 17:30:29 +0800

    feat(update):update

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Frontend</title>
+    <script>
+        let appUrl = '{{ env('API_URL_BE') }}';
+   </script>
 </head>
 <body>
     @viteReactRefresh
