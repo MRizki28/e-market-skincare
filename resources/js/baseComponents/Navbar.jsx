9aa346a650207bfbe28eee6518fee3681d78e582
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-31 20:27:21 +0800

    feat(update):update

--- resources/js/baseComponents/Navbar.jsx
+++ resources/js/baseComponents/Navbar.jsx
@@ -1,68 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { Link } from "react-router-dom";
-import { BsCartCheck } from "react-icons/bs";
 import LogoSkincare from "../../../public/logoEskincare.png";
-import { IoSearch } from "react-icons/io5";
 import { MdAddShoppingCart } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
+import logout from "../scripts/auth/logout";
 
 export default function Navbar() {
+    const [login, setLogin] = useState(false)
+
+    const handleLogout = async () => {
+        await logout.logout(setLogin)
+    }
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            setLogin(true)
+        }
+    }, [])
+
     return (
         <>
-            <nav className=" bg-brownSkincare border-gray-200 font-basicCommersialRegular sticky top-0 z-50">
+            <nav className=" bg-white border-gray-200 font-basicCommersialRegular sticky top-0 z-50">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src={LogoSkincare} className="h-14" alt="Flowbite Logo" />
-                        {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-greyText">R-SKINCARE</span> */}
-                    </a>
-                    <div className="flex md:order-2 space-x-3 md:space-x-4 rtl:space-x-reverse text-white">
-                        <div className="flex justify-center items-center space-x-2 hover:text-greyText">
-                            <CgProfile className="text-2xl"></CgProfile>
-                            <a href={`${appUrl}/cms/admin/login`}>Login</a>
-                        </div>
-                        <div className="hidden lg:block border-white border"></div>
-                        <div className="hidden lg:block">
+                    <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={LogoSkincare} className="object-cover w-[130px] md:w-[180px]" width="180" height="56" alt="Flowbite Logo" />
+                        {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-red-600">R-SKINCARE</span> */}
+                    </Link>
+                    <div className="flex md:order-2 space-x-3 md:space-x-4 rtl:space-x-reverse text-black">
+                        {login === true ? (
+                            <div className="flex justify-center items-center space-x-4 ">
+                                <div className="flex  space-x-2 hover:text-red-600">
+                                    <CgProfile className="text-2xl"></CgProfile>
+                                    <Link to="/profile/"> <span className="text-sm">Muhammad Rizki</span></Link>
+                                </div>
+                                <div className="border-r-4 border-gray-500 h-6"></div>
+                                <div className="flex justify-center items-center">
+                                    <button onClick={handleLogout}>
+                                        <IoIosLogOut className="text-2xl hover:text-red-600"></IoIosLogOut >
+                                    </button>
+                                </div>
 
-                            <div className="flex items-center">
-                                <BsCartCheck className="text-2xl"></BsCartCheck>
+
                             </div>
-                        </div>
-
+                        ) : (
+                            <div className="flex justify-center items-center space-x-2 hover:text-red-600">
+                                <CgProfile className="text-2xl"></CgProfile>
+                                <Link to="/login"> <span className="text-sm">Login</span></Link>
+                            </div>
+                        )}
                     </div>
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border   md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border   md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 text-sm">
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white hover:text-greyText" aria-current="page">Home</a>
+                                <Link to="/" className="block py-2 px-3 text-black hover:text-red-600" aria-current="page">Home</Link>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white hover:text-greyText">Distributor</a>
+                                <a href="#" className="block py-2 px-3 text-black hover:text-red-600">Distributor</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white hover:text-greyText">About</a>
+                                <a href="#" className="block py-2 px-3 text-black hover:text-red-600">About</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
 
-            <div className="lg:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-brownSkincare  border-t ">
+            <div className="lg:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white  border-t ">
                 <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
                     <a href="#" className="inline-flex flex-col items-center justify-center px-5  group">
-                        <FaHome className="text-2xl mb-2 text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText"></FaHome>
-                        <span className="text-sm text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText">Home</span>
+                        <FaHome className="text-2xl mb-2 text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600"></FaHome>
+                        <span className="text-sm text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600">Home</span>
                     </a>
                     <a href="#" className="inline-flex flex-col items-center justify-center px-5  group">
-                        <FaHome className="text-2xl mb-2 text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText"></FaHome>
-                        <span className="text-sm text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText">Distributor</span>
+                        <FaHome className="text-2xl mb-2 text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600"></FaHome>
+                        <span className="text-sm text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600">Distributor</span>
                     </a>
                     <a href="#" className="inline-flex flex-col items-center justify-center px-5  group">
-                        <FaHome className="text-2xl mb-2 text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText"></FaHome>
-                        <span className="text-sm text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText">About</span>
+                        <FaHome className="text-2xl mb-2 text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600"></FaHome>
+                        <span className="text-sm text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600">About</span>
                     </a>
                     <a href="#" className="inline-flex flex-col items-center justify-center px-5  group">
-                        <MdAddShoppingCart className="text-2xl mb-2 text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText"></MdAddShoppingCart>
-                        <span className="text-sm text-white dark:text-white group-hover:text-greyText dark:group-hover:text-greyText">Keranjang</span>
+                        <MdAddShoppingCart className="text-2xl mb-2 text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600"></MdAddShoppingCart>
+                        <span className="text-sm text-black dark:text-black group-hover:text-red-600 dark:group-hover:text-red-600">Keranjang</span>
                     </a>
                 </div>
             </div>
