30bc5283e635b321f962535316d631ffccb10e53
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-31 20:28:23 +0800

    feat(update):update

--- resources/js/ui/home/contents/Hero.jsx
+++ resources/js/ui/home/contents/Hero.jsx
@@ -1,47 +1,46 @@
 import React from "react";
-import HeroImage from "../../../../../public/heroSkincare.webp"
-import HeroImage2 from "../../../../../public/heroSkincare2.webp"
+import HeroImage from "../../../../../public/heroSkincare.webp";
+import HeroImage2 from "../../../../../public/heroSkincare2.webp";
 import Slider from "react-slick";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Hero() {
     const settings = {
-        dots: false,
-        autoplay: true,
+        dots: true,
+        autoplay: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-    }
+    };
 
     return (
-        <div className="">
+        <div className="relative w-full">
             <Slider {...settings}>
-                <div className="custom-w">
+                <div className="custom-w md:w-full">
                     <LazyLoadImage
                         alt="hero"
                         src={HeroImage}
                         effect="blur"
-                        className="object-cover w-full"
+                        className="w-full h-[45vh] lg:h-[70vh] "
                         wrapperProps={{
-                            style: {transitionDelay: "1s"},
-                        }}>
-                        
-                    </LazyLoadImage>
+                            style: { transitionDelay: "1s" },
+                        }}
+                    />
                 </div>
-                <div className="custom-w">
-                <LazyLoadImage
+                <div className="custom-w md:w-full">
+                    <LazyLoadImage
                         alt="hero"
                         src={HeroImage2}
                         effect="blur"
-                        className="object-cover w-full"
+                        className="w-full h-[45vh] lg:h-[70vh] "
                         wrapperProps={{
-                            style: {transitionDelay: "1s"},
-                        }}>
-                    </LazyLoadImage>
+                            style: { transitionDelay: "1s" },
+                        }}
+                    />
                 </div>
             </Slider>
         </div>
-    )
+    );
 }
