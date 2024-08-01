f49887b45321bb8c94e7635ede3b29b989dbb7df
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-01 15:10:41 +0800

    feat(update):update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\CMS\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('check-auth', function () {
+    return response()->json(['data' => true]);
 })->middleware('auth:sanctum');
+
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
