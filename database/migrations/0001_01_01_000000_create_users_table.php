ffa1c79ded90d2076aed317b9b4f20c9f5b40e40
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-01 15:09:50 +0800

    feat(update):update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('role', ['admin', 'user', 'distributor'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
