7b469cecdf453e8130792a9d341494b5793d85d4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-01 15:10:16 +0800

    feat(update):update

--- resources/js/scripts/auth/logout.js
+++ resources/js/scripts/auth/logout.js
@@ -3,20 +3,38 @@ import SweetAlertService from "../../helper/sweetAlert"
 class logout {
     static async logout(setLogin) {
         try {
+            let token = localStorage.getItem('token')
             SweetAlertService.logoutAlert().then(async (result) => {
                 if (result.isConfirmed) {
-                    const response = await axios.post('/logout')
-                    const responseData = response.data
-                    console.log(responseData)
-                    if (responseData.message == 'logout success') {
-                        localStorage.removeItem('token')
-                        setLogin(false)
-                    }
+                    try {
+                        const response = await axios.post('logout', {
+                            headers: {
+                                'Authorization': `Bearer ${token}`
+                            }
+                        })
+                        console.log('disini ajg', response)
+                        const responseData = response.data
+                        if (responseData.message == 'logout success') {
+                            localStorage.removeItem('token')
+                            setLogin(false)
+                            window.location.reload()
+                        } else {
+                            setLogin(true)
+                            window.location.reload()
+                        }
+                    } catch (error) {
+                        if(error.response.status == 401) {
+                            setLogin(true)
+                            SweetAlertService.sessionExpired().then(() => {
+                                localStorage.removeItem('token')
+                                window.location.reload()
+                            })
+                        }
+                    };
                 }
             })
-
         } catch (error) {
-            console.log(error)
+            setLogin(true)
         }
     }
 }
