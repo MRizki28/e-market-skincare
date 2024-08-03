db5f92730c4db4b64f990b234a7565f757f28614
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-03 16:36:26 +0800

    feat(frontend):update frontend

--- app/Repositories/AuthRepositories.php
+++ app/Repositories/AuthRepositories.php
@@ -4,30 +4,35 @@ namespace App\Repositories;
 
 use App\Http\Requests\Auth\AuthRequest;
 use App\Interfaces\AuthInterfaces;
+use App\Models\ProfileModel;
 use App\Models\User;
 use App\Traits\HttpResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthRepositories implements AuthInterfaces
 {
     protected $userModel;
+    protected $profileModel;
     use HttpResponseTrait;
 
-    public function __construct(User $userModel)
+    public function __construct(User $userModel, ProfileModel $profileModel)
     {
         $this->userModel = $userModel;
+        $this->profileModel = $profileModel;
     }
 
     public function login(AuthRequest $request)
     {
         try {
-            if(!Auth::attempt($request->only('email', 'password'))){
+            if (!Auth::attempt($request->only('email', 'password'))) {
                 return response()->json([
                     'status' => 'Unauthorized',
                     'message' => 'email or password is incorrect',
                 ], 401);
-            }else{
+            } else {
                 $user = $this->userModel->with('profile')->where('email', $request->email)->first();
                 $token = $user->createToken('token')->plainTextToken;
                 return response()->json([
@@ -60,6 +65,42 @@ class AuthRepositories implements AuthInterfaces
                 'message' => 'logout success',
             ], 200);
         } catch (\Throwable $th) {
+            return $this->error($th->getMessage());
+        }
+    }
+
+    public function registerUser(AuthRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $user = new $this->userModel;
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->role = $request->input('role');
+            $user->save();
+
+            if($user->role == 'user') {
+                DB::commit();
+                $profile = new $this->profileModel;
+                $profile->id_user = $user->id;
+                $profile->name = $request->input('name');
+                $profile->personal_address = $request->input('personal_address');
+                $profile->personal_phone_number = $request->input('personal_phone_number');
+                $profile->save();
+
+                return $this->success([
+                    'user' => $user,
+                    'profile' => $profile,
+                ],'success', 'register success');
+            }
+            DB::commit();
+
+            return $this->success([
+                'user' => $user,
+            ],'success', 'register success');
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
             return $this->error($th->getMessage());
         }
     }
