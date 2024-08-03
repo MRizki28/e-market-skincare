4d04a3f3bdb08d8bdabab2ad6d6175a3dc1bbdbc
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-03 12:22:33 +0800

    feat(update):update

--- resources/js/scripts/auth/logout.js
+++ resources/js/scripts/auth/logout.js
@@ -1,41 +1,41 @@
+import { useDispatch } from "react-redux"
 import SweetAlertService from "../../helper/sweetAlert"
+import { login, logout as logoutRedux } from "../../redux/slices/checkLogin"
+import store from "../../redux/store"
 
 class logout {
-    static async logout(setLogin) {
-        try {
-            let token = localStorage.getItem('token')
-            SweetAlertService.logoutAlert().then(async (result) => {
-                if (result.isConfirmed) {
-                    try {
-                        const response = await axios.post('logout', {
-                            headers: {
-                                'Authorization': `Bearer ${token}`
-                            }
-                        })
-                        console.log('disini ajg', response)
-                        const responseData = response.data
-                        if (responseData.message == 'logout success') {
+    static async logout() {
+        let token = localStorage.getItem('token')
+        SweetAlertService.logoutAlert().then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    const response = await axios.post(`${appUrl}/logout`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    })
+                    console.log('disini ajg', response)
+                    const responseData = response.data
+                    if (responseData.message == 'logout success') {
+                        localStorage.removeItem('token')
+                        window.location.reload()
+                        store.dispatch(logoutRedux())
+                    } else {
+                        store.dispatch(login())
+                        window.location.reload()
+                    }
+                } catch (error) {
+                    console.log(error)
+                    if (error.response && error.response.status === 401) {
+                        store.dispatch(login())
+                        SweetAlertService.sessionExpired().then(() => {
                             localStorage.removeItem('token')
-                            setLogin(false)
                             window.location.reload()
-                        } else {
-                            setLogin(true)
-                            window.location.reload()
-                        }
-                    } catch (error) {
-                        if(error.response.status == 401) {
-                            setLogin(true)
-                            SweetAlertService.sessionExpired().then(() => {
-                                localStorage.removeItem('token')
-                                window.location.reload()
-                            })
-                        }
-                    };
-                }
-            })
-        } catch (error) {
-            setLogin(true)
-        }
+                        })
+                    }
+                };
+            }
+        })
     }
 }
 
