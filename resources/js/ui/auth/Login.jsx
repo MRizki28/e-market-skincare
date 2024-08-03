5218e194c08f5eb068f2913a1649af67e56d5760
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-03 16:36:53 +0800

    feat(frontend):update frontend

--- resources/js/ui/auth/Login.jsx
+++ resources/js/ui/auth/Login.jsx
@@ -2,14 +2,14 @@ import { Footer } from "../../baseComponents/Footer";
 import Navbar from "../../baseComponents/Navbar";
 import { LoginForm } from "./contents/LoginForm";
 
-export function Auth() {
-    return(
+export function Login() {
+    return (
         <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-            <LoginForm></LoginForm>
-        </main>
-        <Footer />
-    </div>
+            <Navbar />
+            <main className="flex-grow">
+                <LoginForm></LoginForm>
+            </main>
+            <Footer />
+        </div>
     )
 }
