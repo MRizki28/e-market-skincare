b73bc0dbc1899e10eabcc137561d81604f4ebca4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-03 16:36:47 +0800

    feat(frontend):update frontend

--- resources/js/routes/AppRoute.jsx
+++ resources/js/routes/AppRoute.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "../ui/home/Home";
-import { Auth } from "../ui/auth/Login";
+import { Login } from "../ui/auth/Login";
 import { DetailProduct } from "../ui/detailProduct/DetailProduct";
 import NotFound from "../ui/404/404";
 import ProtectedRoutes from "../scripts/protectedRoutes/protectedRoutes";
 import ProtectedLoginRoutes from "../scripts/protectedRoutes/protectedLoginRoutes";
+import { Register } from "../ui/auth/Register";
 
 export default function AppRoute() {
     return (
         <Routes>
-            <Route path="/login" element={<ProtectedLoginRoutes><Auth></Auth></ProtectedLoginRoutes>}></Route>
+            <Route path="/login" element={<ProtectedLoginRoutes><Login></Login></ProtectedLoginRoutes>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
             <Route path="/" element={
                 <Home></Home>
             }></Route>
