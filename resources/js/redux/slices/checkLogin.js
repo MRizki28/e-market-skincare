72145087538696fc4f12c6f72c06e5ce0fc11d9a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-03 12:22:25 +0800

    feat(update):update

---
+++ resources/js/redux/slices/checkLogin.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    isLoggedIn: false
+};
+
+const checkLogin = createSlice({
+    name: 'checkLogin',
+    initialState,
+    reducers: {
+        login: (state) => {
+            state.isLoggedIn = true;
+        },
+        logout: (state) => {
+            state.isLoggedIn = false;
+        },
+        setLoginStatus: (state, action) => {
+            state.isLoggedIn = action.payload;
+        }
+    }
+});
+
+export const { login, logout, setLoginStatus } = checkLogin.actions;
+export default checkLogin.reducer;
+
+export const checkTokenValidity = () => async (dispatch) => {
+    try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(setLoginStatus(false));
+            return;
+        }
+        const response = await axios.get(`${appUrl}/api/check-auth`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        dispatch(setLoginStatus(response.data.data || false));
+    } catch (error) {
+        dispatch(setLoginStatus(false));
+    }
+};
