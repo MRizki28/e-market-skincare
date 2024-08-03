ab432e173bd785bb9614f87cbbee1e2d206a1e4e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-04 01:14:09 +0800

    feat(distributor):create distributor

--- app/Interfaces/DistributorInterfaces.php
+++ app/Interfaces/DistributorInterfaces.php
@@ -9,4 +9,5 @@ interface DistributorInterfaces
 {
     public function getAllData();
     public function createData(DistributorRequest $request);
+    public function getDataForFe(Request $request);
 }
