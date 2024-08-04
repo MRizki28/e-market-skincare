18f77a7d3e668c04c9b25cce6e997373286b9629
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-04 11:33:27 +0800

    feat(distributor):add new field

--- app/Helper/FileUpload.php
+++ app/Helper/FileUpload.php
@@ -11,13 +11,18 @@ class FileUpload
         return $filename;
     }
 
-    public function updateFile($file, $path, $nameFile, $oldFile){
+    public function updateFile($file, $path, $nameFile, $oldFile)
+    {
         $filename = $nameFile . Str::random(5) . '.' . $file->getClientOriginalExtension();
         $file->move(public_path($path), $filename);
-        $old_file = public_path($path) . $oldFile;
-        if (file_exists($old_file)) {
-            unlink($old_file);
+        
+        if ($oldFile) {
+            $oldFilePath = public_path($path) . $oldFile;
+            if (file_exists($oldFilePath) && !is_dir($oldFilePath)) {
+                unlink($oldFilePath);
+            }
         }
+
         return $filename;
     }
 }
