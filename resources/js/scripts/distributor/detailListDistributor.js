48f48551218c1bb310e8666fcc5065951716cad1
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-05 12:58:09 +0800

    feat(update):update

---
+++ resources/js/scripts/distributor/detailListDistributor.js
@@ -0,0 +1,22 @@
+import format from "../../helper/format";
+
+class detailListDistributor {
+    static async getListData(search = '', page, id) {
+        try {
+            const formatHelper = new format()
+            const id_distributor = id
+            const response = await axios.get(`${appUrl}/api/v1/product-by-distributor/${id_distributor}?search=${search}&page=${page}`);
+            const responseData = response.data.data
+            if(response.data.message == 'Success get data product by distributor'){
+                return responseData
+            }else{
+                return []
+            }
+        } catch (error) {
+            console.log(error);
+        };
+        
+    }
+}
+
+export default detailListDistributor;
