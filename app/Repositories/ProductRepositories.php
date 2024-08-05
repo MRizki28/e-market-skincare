aebf7a50b7d462bf9be4f1a4391848612fee93c4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-05 12:57:46 +0800

    feat(update):update

--- app/Repositories/ProductRepositories.php
+++ app/Repositories/ProductRepositories.php
@@ -138,4 +138,29 @@ class ProductRepositories implements ProductInterfaces
             return $this->error($th->getMessage());
         }
     }
+
+    public function getDataByDistributor(Request $request, $id)
+    {
+        $search = $request->input('search');
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+        $page = (int) $request->input('page', 1);
+
+        $query = $this->productModel->where('id_distributor', $id);
+
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('product_name', 'like', '%' . $search . '%')
+                    ->orWhere('price', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        }
+
+        $data = $query->paginate($limit, ['*'], 'page', $page);
+
+        if ($data->isEmpty()) {
+            return $this->dataNotFound();
+        } else {
+            return $this->success($data, 'success', 'Success get data product by distributor');
+        }
+    }
 }
