6fcabd989bbe76d12a58434b3b7d3ccdb8c511f6
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-18 09:58:12 +0800

    feat(order):create API order

---
+++ app/Http/Controllers/CMS/OrderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Order\OrderRequest;
+use App\Repositories\OrderRepositories;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    protected $orderRepositories;
+
+    public function __construct(OrderRepositories $orderRepositories)
+    {
+        $this->orderRepositories = $orderRepositories;
+    }
+    
+    public function getAllData(Request $request)
+    {
+        return $this->orderRepositories->getAllData($request);
+    }
+
+    public function createOrder(OrderRequest $request)
+    {
+        return $this->orderRepositories->createOrder($request);
+    }
+}
