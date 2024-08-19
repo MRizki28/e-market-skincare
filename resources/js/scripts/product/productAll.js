d009f6a10a98fe79281f90a63ec24cd332c6d971
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-19 18:34:40 +0800

    update

--- resources/js/scripts/product/productAll.js
+++ resources/js/scripts/product/productAll.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ProductAll {
     static async getAllData(search, page) {
         try {
-            const response = await axios.get(`${appUrl}/v1/product?search=${search}&page=${page}`)
+            const response = await axios.get(`${appUrl}/v1/product/get-isready?search=${search}&page=${page}`)
             const responseData = response.data
             if (responseData.message == 'Success get data product') {
                 return responseData
