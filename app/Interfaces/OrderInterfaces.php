5722ccdd9c550cb41f24c45936e595b2431511c6
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-09 12:31:35 +0800

    optimize logic payment

--- app/Interfaces/OrderInterfaces.php
+++ app/Interfaces/OrderInterfaces.php
@@ -3,12 +3,14 @@
 namespace App\Interfaces;
 
 use App\Http\Requests\Order\OrderRequest;
+use App\Http\Requests\PrepareOrder\PrepareOrderRequest;
 use Illuminate\Http\Request;
 
 interface OrderInterfaces
 {
     public function getAllData(Request $request);
-    public function createOrder (OrderRequest $request);
+    public function prepareOrder (PrepareOrderRequest $request);
+    public function createOrder(OrderRequest $request);
     public function orderNotification(Request $request);
     public function historyOrder(Request $request);
 }
