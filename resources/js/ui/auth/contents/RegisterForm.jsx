10c31a8cc6500761b3059820cbf2d10a6e81a466
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-11 11:11:07 +0800

    fix bug and add middleware role

--- resources/js/ui/auth/contents/RegisterForm.jsx
+++ resources/js/ui/auth/contents/RegisterForm.jsx
@@ -7,35 +7,24 @@ import SweetAlertService from "../../../helper/sweetAlert";
 
 export function RegisterForm() {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const [distributorRole, setDistributorRole] = useState(false);
-
-    const handleChangeRole = async (e) => {
-        if (e.target.value === 'distributor') {
-            setDistributorRole(true)
-        } else {
-            setDistributorRole(false)
-        }
-    }
-
-    console.log(distributorRole)
 
     const onSubmit = () => async (data) => {
         try {
             const response = await axios.post(`${appUrl}/api/v1/register-user`, data)
             const responseData = response.data
             console.log(responseData)
-            if(responseData.message == 'register success'){
+            if (responseData.message == 'register success') {
                 SweetAlertService.successRegister().then(() => {
                     window.location.href = '/login'
                 })
             }
         } catch (error) {
             console.log(error)
-            if(error.response && error.response.data.data.password == 'The password field confirmation does not match.'){
+            if (error.response && error.response.data.data.password == 'The password field confirmation does not match.') {
                 SweetAlertService.passwordNotMatch()
-            }else if(error.response && error.response.data.data.email == 'The email has already been taken.'){
+            } else if (error.response && error.response.data.data.email == 'The email has already been taken.') {
                 SweetAlertService.emailAlreadyExist()
-            }else{
+            } else {
                 SweetAlertService.errorAlert()
             }
         }
@@ -58,14 +47,13 @@ export function RegisterForm() {
                                 <p>email</p>
                             </div>
                             <form onSubmit={handleSubmit(onSubmit())} className="space-y-4">
-                            <div>
+                                <div>
                                     <label htmlFor="role" className="block text-sm font-medium text-gray-700">Type Akun</label>
                                     <select
                                         id="role"
                                         {...register('role', { required: "Type akun wajib diisi" })}
                                         className={`mt-1 p-2 w-full border rounded-md outline-none transition-colors duration-300 ${errors.role ? 'border-red-500' : 'border-gray-300'}`}
                                         defaultValue=""
-                                        onChange={handleChangeRole}
                                     >
                                         <option value="user">User</option>
                                         <option value="distributor">Distributor</option>
@@ -113,49 +101,47 @@ export function RegisterForm() {
                                         <p className="text-red-500 text-xs mt-1">{errors.password_confirmation.message}</p>
                                     )}
                                 </div>
-                                {!distributorRole && (
-                                    <>
-                                        <div>
-                                            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nama</label>
-                                            <input
-                                                type="text"
-                                                id="name"
-                                                {...register("name", { required: "Nama wajib diisi" })}
-                                                className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.name ? 'border-red-500' : 'border-gray-300'
-                                                    }`}
-                                            />
-                                            {errors.name && (
-                                                <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>
-                                            )}
-                                        </div>
-                                        <div>
-                                            <label htmlFor="personal_address" className="block text-sm font-medium text-gray-700">Alamat</label>
-                                            <input
-                                                type="text"
-                                                id="personal_address"
-                                                {...register("personal_address", { required: "Alamat wajib diisi" })}
-                                                className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.personal_address ? 'border-red-500' : 'border-gray-300'
-                                                    }`}
-                                            />
-                                            {errors.personal_address && (
-                                                <p className="text-red-500 text-xs mt-1">{errors.personal_address.message}</p>
-                                            )}
-                                        </div>
-                                        <div>
-                                            <label htmlFor="personal_phone_number" className="block text-sm font-medium text-gray-700">Nomor Handphone</label>
-                                            <input
-                                                type="number"
-                                                id="personal_phone_number"
-                                                {...register("personal_phone_number", { required: "Nomor hp wajib diisi" })}
-                                                className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.personal_phone_number ? 'border-red-500' : 'border-gray-300'
-                                                    }`}
-                                            />
-                                            {errors.personal_phone_number && (
-                                                <p className="text-red-500 text-xs mt-1">{errors.personal_phone_number.message}</p>
-                                            )}
-                                        </div>
-                                    </>
-                                )}
+                                <>
+                                    <div>
+                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nama</label>
+                                        <input
+                                            type="text"
+                                            id="name"
+                                            {...register("name", { required: "Nama wajib diisi" })}
+                                            className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.name ? 'border-red-500' : 'border-gray-300'
+                                                }`}
+                                        />
+                                        {errors.name && (
+                                            <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <label htmlFor="personal_address" className="block text-sm font-medium text-gray-700">Alamat</label>
+                                        <input
+                                            type="text"
+                                            id="personal_address"
+                                            {...register("personal_address", { required: "Alamat wajib diisi" })}
+                                            className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.personal_address ? 'border-red-500' : 'border-gray-300'
+                                                }`}
+                                        />
+                                        {errors.personal_address && (
+                                            <p className="text-red-500 text-xs mt-1">{errors.personal_address.message}</p>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <label htmlFor="personal_phone_number" className="block text-sm font-medium text-gray-700">Nomor Handphone</label>
+                                        <input
+                                            type="number"
+                                            id="personal_phone_number"
+                                            {...register("personal_phone_number", { required: "Nomor hp wajib diisi" })}
+                                            className={`mt-1 p-2 w-full border rounded-md   outline-none transition-colors duration-300 ${errors.personal_phone_number ? 'border-red-500' : 'border-gray-300'
+                                                }`}
+                                        />
+                                        {errors.personal_phone_number && (
+                                            <p className="text-red-500 text-xs mt-1">{errors.personal_phone_number.message}</p>
+                                        )}
+                                    </div>
+                                </>
                                 <div>
                                     <button type="submit" className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800  focus:bg-black  focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign In</button>
                                 </div>
