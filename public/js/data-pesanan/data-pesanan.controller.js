b9fdfd2d388936b4faa1708548f1e489be79eba8
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-16 15:51:35 +0800

    update data order on distributor role

--- public/js/data-pesanan/data-pesanan.controller.js
+++ public/js/data-pesanan/data-pesanan.controller.js
@@ -4,4 +4,27 @@ $(document).ready(function () {
     const DataPesananService = new dataPesananService()
     
     DataPesananService.getDataPesanan();
+
+
+    $('#form-search').on('keyup', function (e) {
+        if(keyCode === 13) {
+            DataPesananService.getDataPesanan();
+        }
+    });
+
+    $('#form-search').on('change', function () {
+        DataPesananService.getDataPesanan();
+    });
+
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault();
+        DataPesananService.getDataPesanan();
+    });
+
+    $(document).on('click', '.delete-confirm', function (e) {
+        e.preventDefault();
+        const id = $(this).data('id');
+        DataPesananService.deleteData(id);
+    })
+
 });
