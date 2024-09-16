1558270840f2089b41237b29e3ae17fc45ffcf55
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-16 15:51:40 +0800

    update data order on distributor role

--- public/js/data-pesanan/data-pesanan.service.js
+++ public/js/data-pesanan/data-pesanan.service.js
@@ -1,10 +1,11 @@
 
 class dataPesananService {
+
     async getDataPesanan(url) {
         const pagination = $('.pagination');
         const table = $('#table tbody');
         const dataNotFound = $('#dataNotFound');
-        const totalData = $('#totalData');
+        const totalData = $('#data-total');
 
         let params = $('#form-search').val();
         let endpoint = paramsUrl(url || '/v1/order/get-by-distributor', { search: params });
@@ -19,18 +20,19 @@ class dataPesananService {
 
         if (responseData.message === 'success get data order by distributor') {
             $.each(responseData.data.data, function (index, item) {
+                let personalPhoneNumber = item.profile.personal_phone_number;
                 tableBody += "<tr>";
                 tableBody += "<td>" + item.product.product_name + "</td>";
                 tableBody += "<td>" + item.profile.name + "</td>";
                 tableBody += "<td>" + formatDate(item.created_at) + "</td>";
                 tableBody += "<td>Rp." + numberWithCommas(item.price) + "</td>";
-                tableBody += "<td>" + item.profile.personal_phone_number + "</td>";
+                tableBody += "<td>" + `<a href="https://wa.me/${personalPhoneNumber}" target=_blank><i class="fab fa-whatsapp fa-xl text-success "></i></a>` + "</td>";
                 tableBody += "<td>" + item.quantity + "</td>";
-                tableBody += "<td>Rp." + numberWithCommas(item.total_price ) + "</td>";
+                tableBody += "<td>Rp." + numberWithCommas(item.total_price) + "</td>";
+                tableBody += "<td>" + statusInfo(item.status) + "</td>";
                 tableBody +=
                     "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                    "<button class='btn btn-sm edit-modal mr-1' data-toggle='modal' data-target='#productModal' data-id='" +
-                    item.id + "'><i class='fas fa-edit'></i></button>" +
+
                     "<button type='submit' class='delete-confirm btn btn-sm' data-id='" +
                     item.id + "'><i class='fas fa-trash-alt'></i></button>" +
                     "</td>";
@@ -46,8 +48,20 @@ class dataPesananService {
             pagination.empty();
             totalData.text('0');
         }
+    }
 
-
+    async deleteData(id) {
+        deleteAlert().then(async (result) => {
+            if (result.isConfirmed) {
+                const response = await axios.delete(`/v1/order/delete/${id}`);
+                if (response.data.message === 'success delete order') {
+                    successAlert(response.data.message);
+                    dataPesananService.getDataPesanan();
+                } else {
+                    errorAlert(response.data.message);
+                }
+            }
+        })
     }
 }
 
