ea6003ab3626cbabe566163daf3922f0ca99f862
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-20 09:33:34 +0800

    fix footer

--- resources/js/baseComponents/Footer.jsx
+++ resources/js/baseComponents/Footer.jsx
@@ -22,22 +22,16 @@ export function Footer() {
                     </Link>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            <Link to="about" className="hover:underline me-4 md:me-6">About</Link>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="https://wa.me/082290333669?text=Halo%20admin" target="_blank" className="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    © 2024 MR All Rights Reserved.
                 </span>
             </div>
         </footer>
