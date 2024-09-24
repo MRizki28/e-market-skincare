408dfc27601e123a3c4949c5d9f6c352bdd03d91
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-24 21:52:06 +0800

    add product management in admin

--- resources/views/components/base-header.blade.php
+++ resources/views/components/base-header.blade.php
@@ -1,3 +1,4 @@
+@props(['headerIcon', 'headerName'])
 <div>
     <div class="page-header d-flex justify-content-between align-items-center">
         <h1 class="page-title"><i class="{{ $headerIcon }} mr-2"></i>{{ $headerName }}</h1>
