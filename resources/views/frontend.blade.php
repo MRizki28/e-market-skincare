2106eeed9ca47bb63d39370e29387f241bde5067
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-25 10:52:43 +0800

    finish

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Frontend</title>
+    <link rel="icon" href="{{ asset('logo2.png') }}" type="image/svg+xml" />
+    <title>E-SKINCARE</title>
     <script>
         let appUrl = '{{ env('API_URL_BE') }}';
    </script>
