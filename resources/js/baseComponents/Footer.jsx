b0a86c181bf9df868dd3a93cdd072441dd2eb9e0
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-25 10:52:29 +0800

    finish

--- resources/js/baseComponents/Footer.jsx
+++ resources/js/baseComponents/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import LogoEskincare from "../../../public/logoEskincare.png";
+import LogoEskincare from "../../../public/logo2.png";
 import { Link } from "react-router-dom";
 
 export function Footer() {
